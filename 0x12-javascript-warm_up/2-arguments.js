9614a2ba5b283b5019254242741aeeaff9d0fee4
Smolclin <clinsmol10@gmail.com>
2024-04-23 14:47:32 -0700

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const argueLength = process.argv.length;
+
+if (argueLength === 2) {
+  console.log('No argument');
+} else if (argueLength === 3) {
+  console.log('Arguement found');
+} else {
+  console.log('Arguement found');
+}
