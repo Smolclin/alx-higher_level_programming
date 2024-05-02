26699390671b7567305aecdb170e1dffd2a479c7
Smolclin <clinsmol10@gmail.com>
2024-05-02 08:23:07 -0700

    Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY statees.id ASC")
+    rows = cur.fetchall()
+
+    for row in rows:
+        if row[1].startswitch("N"):
+            print(row)
+
+    cur.close()
+    conn.close()
