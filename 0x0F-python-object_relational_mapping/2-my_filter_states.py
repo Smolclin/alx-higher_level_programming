299b8c146b0b7bfcc88875f9ca611b0d3abeba16
Smolclin <clinsmol10@gmail.com>
2024-05-03 09:47:19 -0700

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
+'''
+This script takes arguement and
+displays all values in the states
+where name matches the arguement
+from the database hbtn_0e_0_usa
+'''
 
 import MySQLdb
 from sys import argv
 
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=argv[1], passwd=argv[2], db=argv[3])
+    '''
+    Access to the database and get the states
+    from the database
+    '''
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
-                    states.id ASC".format(argv[4]))
+    cur.execute("SELECT * FROM states \
+                 WHERE name LIKE BINARY '{}' \
+                 ORDER BY states.id ASC".format(argv[4]))
     rows = cur.fetchall()
+    state_dict = {}
 
     for row in rows:
-        print(row)
+        state_id, state_name = row
+        if state_name not in state_dict:
+            print(row)
+            state_dict[state_name] = True
