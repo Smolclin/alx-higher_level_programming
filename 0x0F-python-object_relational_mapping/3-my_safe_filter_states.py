3c9fdb0b97e8596c9df8813e701439007cbe67c9
Smolclin <clinsmol10@gmail.com>
2024-05-03 14:14:46 -0700

    SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
+'''
+The above script takes an arguement and
+displays all values in the states
+where name matches the arguement
+from the database hbtn_0e_0_usa
+the script is safe from MySQLdb injections
+'''
 
 import MySQLdb as db
 from sys import argv
 
 
 if __name__ == "__main__":
+    '''
+    Access to the database and get the states
+    from the database
+    '''
     db_connect = db.connect(host="localhost", port=3306,
                             user=argv[1], passwd=argv[2], db=argv[3])
 
@@ -15,5 +26,10 @@ if __name__ == "__main__":
 
     rows_selected = db_cursor.fetchall()
 
+    state_dict = {}
+
     for row in rows_selected:
-        print(row)
+        state_id, state_name = row
+        if state_name not in state_dict:
+            print(row)
+            state_dict[state_name] = True
