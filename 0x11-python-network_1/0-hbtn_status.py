7289736bfcea27d6f803246cb2e647ade3164add
Smolclin <clinsmol10@gmail.com>
2024-05-10 04:52:01 -0700

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,9 +2,9 @@
 ''' Python script that fetches https://alx-intranet.hbtn.io/status '''
 from urllib import request
 
-if __name__ == "__main__":
-    with request.urlopen('https://intranet.hbtn.io/status') as page:
-        content = page.read()
+url = 'http://intranet.hbtn.oi/status'
+with request.urlopen(url) as response:
+    content = response.read()
     print('Body response:')
     print('\t- type: {}'.format(type(content)))
     print('\t- content: {}'.format(content))
