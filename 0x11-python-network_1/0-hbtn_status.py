89e2fcbeb3aa6875b062acf6b5bdb80c96a3095c
Smolclin <clinsmol10@gmail.com>
2024-05-11 08:14:31 -0700

    Fix the bug

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,10 +2,10 @@
 ''' Python script that fetches https://alx-intranet.hbtn.io/status '''
 from urllib import request
 
-url = 'http://intranet.hbtn.oi/status'
-with request.urlopen(url) as response:
+if __name__ == "__main__":
+with request.urlopen("https://intranet.hbtn.io/status") as response:
     content = response.read()
     print('Body response:')
     print('\t- type: {}'.format(type(content)))
     print('\t- content: {}'.format(content))
-    print('\t- utf8 content: {}'.format(content.decode('utf-8')))
+    print('\t- utf8 content: {}'.format(content.decode(encoding='utf-8')))
