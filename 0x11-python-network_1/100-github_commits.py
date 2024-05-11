7ef53f4cb9f6a701ffd79072cb6b82a8b52e29a6
Smolclin <clinsmol10@gmail.com>
2024-05-11 07:19:18 -0700

    Time for an interview!

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' script that takes 2 arguments in order to solve a challenge '''
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    req = requests.get(url)
+    commits = req.json()
+    try:
+        for g in range(10):
+            print("{}: {}".format(
+                commits[g].get("sha"),
+                commits[g].get("commit").get("auther").get("name")))
+    except IndexError:
+        pass
