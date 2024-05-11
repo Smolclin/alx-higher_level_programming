d5e7048ac3fad6e6ad734a7d04b1559aaee54a83
Smolclin <clinsmol10@gmail.com>
2024-05-11 05:31:42 -0700

    Fix error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,15 +2,12 @@
 ''' Python script that takes in a URL, sends a request to
 the URL and displays the body of the response '''
 
-from urllib import request, error
-import requests
+from urllib import request, error, parse
+import sys
 
 if __name__ == '__name__':
-    url = sys.argv[1]
-
-    my_req = request.Request(url)
     try:
         with request.urlopen(sys.argv[1]) as response:
-            print(response.read().decode("utf8"))
+            print(response.read().decode("utf-8"))
     except error.HTTPError as error:
         print("Error code: {}".format(error.code))
