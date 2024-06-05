3cc2fffeea9de30a6af66d39e0eb7f4fc7c2e9bf
Smolclin <clinsmol10@gmail.com>
2024-06-04 17:13:57 -0700

    Who was playing in this movie?

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const urlApi = 'https://swapi.co/api/films/' + process.argv[2];
+
+request(urlApi, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const charUrl = JSON.parse(body).characters;
+
+  for (let j = 0; j < charUrl.length; j++) {
+    request(charUrl[j], function (error, response, body) {
+      if (error) {
+        console.error(error);
+      }
+      console.log(JSON.parse(body).name);
+    });
+  }
+});
